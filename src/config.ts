e184f274029da775fced10f9bf1486cad1d9135d
Arian Saputra <rinaariansaputra@gmail.com>
2025-01-21 00:10:16 +0800

    initial project

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+export class Config {
+    public static readonly Port: number = 8872;
+    public static readonly Env: string = 'dev';
+
+    public static readonly DB_HOST: string;
+    public static readonly DB_PORT: number;
+    public static readonly DB_NAME: string;
+    public static readonly DB_USER: string;
+    public static readonly DB_PASSWORD: string; 
+}
---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express, { Express } from "express";
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import morgan from 'morgan';
+
+import { Config } from "./config";
+
+export class Server {
+    private rest: Express = express();
+
+    public restRoute() {
+        this.rest.use(bodyParser.json());
+        this.rest.use(cors());
+        this.rest.use(morgan(Config.Env));
+    }
+
+    public start(){
+        this.rest.listen(Config.Port, () => {
+            console.log(`Task Management Service running on http://localhost:${Config.Port}`);
+        });
+    }
+};
