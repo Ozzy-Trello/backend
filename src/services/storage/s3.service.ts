fb6dbaac5524abda47b036b46c872246d4b3a653
Fishy <garry@thesignpack.com>
2025-01-22 15:11:53 +0700

    use aws sdk s3

--- src/services/storage/s3.service.ts
+++ src/services/storage/s3.service.ts
@@ -1,22 +1,20 @@
-import * as AWS from "aws-sdk";
+import { ListBucketsCommand, S3Client } from "@aws-sdk/client-s3";
 
-export const s3 = new AWS.S3({
-  accessKeyId: process.env.S3_ACCESS_KEY,
-  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-  region: process.env.S3_REGION,
-  endpoint: process.env.S3_ENDPOINT,
+const accessKeyId = process.env.S3_ACCESS_KEY;
+const secretAccessKey = process.env.S3_SECRET_ACCESS_KEY;
+const region = process.env.S3_REGION;
+const endpoint = process.env.S3_ENDPOINT;
+
+if (!accessKeyId || !secretAccessKey || !region || !endpoint) {
+  throw new Error("Missing required environment variables");
+}
+
+export const s3 = new S3Client({
+  region: region,
+  endpoint: endpoint,
+  credentials: {
+    accessKeyId: accessKeyId,
+    secretAccessKey: secretAccessKey,
+  },
+  forcePathStyle: true,
 });
-
-export const inits3 = async () => {
-  try {
-    s3.listBuckets(function (err, data) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(data.Buckets);
-      }
-    });
-  } catch (error) {
-    console.error(error);
-  }
-};
--- src/services/storage/upload.service.ts
+++ src/services/storage/upload.service.ts
@@ -1,11 +1,28 @@
+import {
+  PutObjectCommand,
+  ObjectCannedACL,
+  PutObjectCommandInput,
+} from "@aws-sdk/client-s3";
 import { s3 } from "./s3.service";
 
 export const UploadFile = async (file: any) => {
-  const uploadParams = {
+  const uploadParams: PutObjectCommandInput = {
     Bucket: "ozzy-trello",
     Body: file.buffer,
     Key: file.originalname,
+    ACL: ObjectCannedACL.public_read,
   };
-  const res = await s3.upload(uploadParams).promise();
-  return res;
+
+  try {
+    const command = new PutObjectCommand(uploadParams);
+
+    const res = await s3.send(command);
+
+    const fileUrl = `${process.env.S3_ENDPOINT}/${uploadParams.Bucket}/${uploadParams.Key}`;
+
+    return { ...res, fileUrl };
+  } catch (error) {
+    console.error("Error uploading file:", error);
+    throw error;
+  }
 };
