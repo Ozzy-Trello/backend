951999c98aba8efb8bc0cfe8e618ba459b785d91
Arian Saputra <rinaariansaputra@gmail.com>
2025-02-07 02:01:22 +0800

    swagger auth

--- src/views/rest/swagger.ts
+++ src/views/rest/swagger.ts
@@ -1,20 +1,29 @@
 export const options = {
-    definition: {
-        openapi: "3.1.0",
-        info: {
-            title: "Task management RestAPI",
-            version: "0.1.0",
-            description:
-                "This is a simple task management application made with Express and documented with Swagger",
-        },
-        servers: [
-            {
-                url: "http://localhost:8872",
-            },
-            {
-                url: "http://103.150.100.46:8872",
-            },
-        ],
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "Task management RestAPI",
+      version: "0.1.0",
+      description:
+        "This is a simple task management application made with Express and documented with Swagger",
     },
-    apis: ["./src/views/rest/docs.ts"],
+    servers: [
+      {
+        url: "http://localhost:8872",
+      },
+      {
+        url: "http://103.150.100.46:8872",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        BearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT"
+        }
+      }
+    },
+  },
+  apis: ["./src/views/rest/docs.ts"],
 };
