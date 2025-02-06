c8c2a24c515fcbe85f4e79d9a649c6f7b27e4cf4
Arian Saputra <rinaariansaputra@gmail.com>
2025-02-07 00:34:21 +0800

    auth route

--- src/views/rest/auth_view.ts
+++ src/views/rest/auth_view.ts
@@ -1,15 +1,18 @@
-import { AuthRestViewI } from "@/views/rest/interfaces";
+import {AuthRestViewI} from "@/views/rest/interfaces";
 import {Request, Response} from "express";
 
 
 export default class AuthRestView implements AuthRestViewI {
     Login(req: Request, res: Response): void {
+        res.json({ message: 'post login route router' });
     }
 
     RefreshToken(req: Request, res: Response): void {
+        res.json({ message: 'patch token route router' });
     }
 
     Register(req: Request, res: Response): void {
+        res.json({ message: 'post register route router' });
     }
 
 }
