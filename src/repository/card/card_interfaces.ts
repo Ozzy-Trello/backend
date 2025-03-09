8c3d55daeeacfbe4ea768159f78b871ba217e0d9
Arian Saputra <rinaariansaputra@gmail.com>
2025-03-09 17:43:09 +0800

    card repo

--- src/repository/card/card_interfaces.ts
+++ src/repository/card/card_interfaces.ts
@@ -1,24 +1,36 @@
-import {ResponseData} from "@/utils/response_utils";
+import {ResponseData, ResponseListData} from "@/utils/response_utils";
+import {Paginate} from "@/utils/data_utils";
 
 export interface CardRepositoryI {
   getCard(filter: filterCardDetail): Promise<ResponseData<CardDetail>>;
   createCard(data: CardDetail): Promise<ResponseData<CardDetail>>;
   deleteCard(filter: filterCardDetail): Promise<number>;
   updateCard(filter: filterCardDetail, data: CardDetailUpdate): Promise<number>;
-  getCardList(filter: filterCardDetail): Promise<Array<CardDetail>>;
+  getListCard(filter: filterCardDetail, paginate: Paginate): Promise<ResponseListData<Array<CardDetail>>>;
 }
 
 export interface filterCardDetail {
   id?: string;
-  list_id?: string;
   name?: string;
   description?: string;
+  list_id?: string;
+
+  __orId?: string;
+  __orName?: string;
+  __orDescription?: string;
+  __orListId?: string;
+
+  __notId?: string;
+  __notName?: string;
+  __notDescription?: string;
+  __notListId?: string;
 }
 
 export class CardDetailUpdate {
-  public list_id?: string;
   public name?: string;
   public description?: string;
+  public list_id?: string;
+  public order?: number;
 
   constructor(payload: Partial<CardDetailUpdate>) {
     Object.assign(this, payload);
@@ -26,18 +38,20 @@ export class CardDetailUpdate {
 
   public toObject(): any {
     const data: any = {};
-    if (this.list_id) data.list_id = this.list_id;
     if (this.name) data.name = this.name;
     if (this.description) data.description = this.description;
+    if (this.list_id) data.list_id = this.list_id;
+    if (this.order) data.order = this.order;
     return data
   }
 }
 
 export class CardDetail {
-  public id?: string;
-  public list_id?: string;
+  public id!: string;
   public name?: string;
-  public description?: string;
+  public description!: string;
+  public list_id!: string;
+  public order!: number;
 
   constructor(payload: Partial<CardDetail>) {
     Object.assign(this, payload);
