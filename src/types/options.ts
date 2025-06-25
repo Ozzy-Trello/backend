ed1de1fc6a63137bc4e46f5fcdcf828381299d38
Garry <79770672+garryfishy@users.noreply.github.com>
2025-06-26 00:50:23 +0700

    Feat/automations rules (#103)

--- src/types/options.ts
+++ src/types/options.ts
@@ -1,12 +1,14 @@
 export enum EnumOptionPosition {
   BottomOfList = "bottom-of-list",
-  TopOfList = "top-of-list"
+  TopOfList = "top-of-list",
 }
 
 export enum EnumOptionsNumberComparisonOperators {
   Exactly = "exactly",
   FewerThan = "fewer-than",
   MoreThan = "more-than",
+  MoreOrEqual = "greater-or-equal-to",
+  FewerOrEqual = "lower-or-equal-to",
 }
 
 export enum EnumOptionsSubject {
@@ -14,5 +16,9 @@ export enum EnumOptionsSubject {
   ByMe = "by-me",
   BySpecificUser = "by-specific-user",
   ByAnyoneExceptMe = "by-anyone-except-me",
-  ByAnyoneExceptSpecificUser = "by-anyone-except-specific-user"
+  ByAnyoneExceptSpecificUser = "by-anyone-except-specific-user",
+}
+
+export enum EnumOptionsSet {
+  Cleared = "cleared",
 }
